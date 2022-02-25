36acc7ecdf7ffa7062b2525324e5bee92ab5afa1
LARRYDDEV <wajuore456@gmail.com>
2022-02-25 14:32:14 +0100

    the extra resources in react class

--- src/App.js
+++ src/App.js
@@ -7,9 +7,13 @@ import Cards from './Components/Cards';
 import Portfolio from './Components/Portfolio';
 import Contacts from './Components/Contacts'
 import Footer from './Components/Footer';
-
+import Car from './Components/Class';
+import Functions from './Components/Class/Function';
+import Class from './Components/Class/Class';
+import Counter from './Components/Counter';
 
 function App() {
+  
   return (
     <div>
       <Navbar/>
@@ -19,6 +23,11 @@ function App() {
       <Portfolio/>
       <Contacts/>
       <Footer/>
+      {/* <Car/> */}
+      
+      {/* <Functions title="hello guys" real={true} array={Array}/>
+      <Class title="mr david" /> */}
+      {/* <Counter/> */}
     </div>
   );
 }
